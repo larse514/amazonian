cc202f4d85225d3f7487b20d8115735d37e28c6d
andrewlarsen <andrew.larsen@concordusa.com>
2018-02-18 10:00:14 -0600

    add commandLine parser logic

--- commandline/commandLine.go
+++ commandline/commandLine.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	textPtr := flag.String("text", "", "Text to parse. (Required)")
+	metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+	uniquePtr := flag.Bool("unique", false, "Measure unique values of a metric.")
+	flag.Parse()
+
+	if *textPtr == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
 }
