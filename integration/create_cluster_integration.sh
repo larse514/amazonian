dcb3266c2f9d8491cc1774c20082a6a67da8e195
andrewlarsen <andrew.larsen@concordusa.com>
2018-02-25 17:27:14 -0600

    refactor integration tests to not require existing cluster

--- integration/create_cluster_integration.sh
+++ integration/create_cluster_integration.sh
@@ -6,10 +6,22 @@ CONTAINER_NAME=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f' | head -c 5`
 CLUSTER_NAME=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f' | head -c 5`
 
 PRIORITY=`cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2`
+
 ./workdir/amazonian --VPC=vpc-c7aa77be --Priority=${PRIORITY} --HostedZoneName=vssdevelopment.com \
 --Image=willejs/go-hello-world --ServiceName=${CONTAINER_NAME} --ContainerName=${CONTAINER_NAME} \
 --ClusterName=${CLUSTER_NAME} --ClusterExists=false --Subnets=subnet-b61d81fe,subnet-0202dc58 --KeyName=dummy_key1 \
 ClusterSize=1 mazSizePrt=1 instanceTypePrt=t2.medium
+
+curl --fail https://${CONTAINER_NAME}.vssdevelopment.com/
+
+aws cloudformation delete-stack --stack-name "${CONTAINER_NAME}"
+
+##Now test and ensure it can reuse the same cluster
+CONTAINER_NAME=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f' | head -c 5`
+PRIORITY=`cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2`
+./workdir/amazonian --VPC=vpc-c7aa77be --Priority=${PRIORITY} --HostedZoneName=vssdevelopment.com \
+--Image=willejs/go-hello-world --ServiceName=${CONTAINER_NAME} --ContainerName=${CONTAINER_NAME} \
+--ClusterName=${CLUSTER_NAME} --ClusterExists
 
 curl --fail https://${CONTAINER_NAME}.vssdevelopment.com/
 
